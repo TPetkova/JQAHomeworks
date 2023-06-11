6cab17b530bc3808844416a16016d65aafbc51b4
Teodora Petkova <tedy_vd@abv.bg>
2023-06-11 21:58:51 +0300

    commit homework 9

---
+++ src/main/java/Homework8/BaseTest.java
@@ -0,0 +1,23 @@
+package Homework8;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
--- src/main/java/Homework8/FirstTestCase.java
+++ src/main/java/Homework8/FirstTestCase.java
@@ -1,20 +1,12 @@
 package Homework8;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstTestCase {
-    private WebDriver driver;
+public class FirstTestCase extends BaseTest {
 
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
-    }
-
+    @Test
     public void login() {
         WebElement username = driver.findElement(By.id("user-name"));
         username.sendKeys("standard_user");
@@ -22,20 +14,5 @@ public class FirstTestCase {
         password.sendKeys("secret_sauce");
         WebElement loginButton = driver.findElement(By.id("login-button"));
         loginButton.click();
-
     }
-
-    public void closeBrowser() {
-        driver.quit();
-    }
-
-    public static void main(String args[]) {
-        FirstTestCase test = new FirstTestCase();
-        test.setUp();
-        test.login();
-        test.closeBrowser();
-    }
-
-
-
 }
